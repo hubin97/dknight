f9cab3a175610d96017b4092f1e1a0ad9adaaaca
hubin.h <hubin.huang@rootglobal.net>
2024-10-02 12:39:31 +0800

    解决梯子形状问题(唯一化)

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,19 +6,26 @@ const JUMP_VELOCITY = -250.0
 @onready var ray_cast_ladder = $RayCastLadder
 @onready var animated_sprite = $AnimatedSprite2D
 
+# 是否在梯子上
+var on_ladder: bool = false
+# 梯子水平速度
+const climb_speed_h = SPEED * 4/5
+# 梯子锤子速度
+const climb_speed_v = SPEED / 2
+
 func _physics_process(delta):
 	
-	if ray_cast_ladder.get_collider():
+	if on_ladder:
 		_ladder_climb(delta)
 	else:
 		_idle_and_move(delta)
 		
 	move_and_slide()
 
-# 正常运动
+# 常规
 func _idle_and_move(delta):
 		# Add the gravity.
-	if not is_on_floor():
+	if not is_on_floor() and !on_ladder:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
@@ -39,15 +46,29 @@ func _idle_and_move(delta):
 	elif velocity.x < 0:
 		animated_sprite.flip_h = true
 
-#爬梯子
+# 爬梯子
 func _ladder_climb(_delta):
-	print("_ladder_climb")
-	var direction = Vector2.ZERO
-	direction.x = Input.get_axis("move_left", "move_right")
-	direction.y = Input.get_axis("move_up", "move_down")
-	
+	#print("_ladder_climb")
+	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
-		velocity = direction * SPEED/2 
+		velocity.x = direction * climb_speed_h
 	else:
-		velocity = Vector2.ZERO
-	
+		velocity.x = move_toward(velocity.x, 0, climb_speed_h)
+		 
+	if Input.is_action_pressed("move_down"):
+		velocity.y = climb_speed_v
+	elif Input.is_action_pressed("move_up"):
+		velocity.y = -climb_speed_v
+	else:
+		velocity.y = 0
+		
+func _on_ladder_body_entered(body):
+	print(body.name)
+	if body.name == 'Player':
+		print("进入梯子区域")
+		on_ladder = true
+
+func _on_ladder_body_exited(body):
+	if body.name == 'Player':
+		print("跳出梯子区域")
+		on_ladder = false
